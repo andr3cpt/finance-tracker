492be03de2d2e8c027d8bc0d2aab1308f2fc29ba
Andrea Caputo <andreacaputo712@gmail.com>
2026-04-30 18:57:51 +0200

    Added pseudocode for user input/adding dict rows

--- tracker.py
+++ tracker.py
@@ -13,5 +13,13 @@ if not os.path.exists(FILE_NAME):
 else:
     print(f"File: {FILE_NAME} not found.")
 
+# --- To do ---
+# 1. Prompt user for 'item' and 'category' (they're both strings)
+# 2. Use while loop to ask for 'price'
+# 3. Try: parse price as float() --> if succesful: break
+# 4. Except: ValueError --> print("Not valid!") --> re-prompt user
+# 4. Open 'expenses.csv' in "a" mode ("append")
+# 5. Write new data as a dictionary row ("item": item, "price": price, "category": category)
+
 
         
