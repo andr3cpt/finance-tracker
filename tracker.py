4e6198045b36b52f8fb0c274a8dda3d8a003de61
Andrea Caputo <andreacaputo712@gmail.com>
2026-04-30 18:14:35 +0200

    Added OS file check and printed logs

--- tracker.py
+++ tracker.py
@@ -2,10 +2,14 @@ import os
 import csv
 
 
-with open("expenses.csv", "w") as file:
-    writer = csv.DictWriter(file, fieldnames=["item", "price", "category"])
-    writer.writeheader()
-    print("File created successfully")
+if not os.path.exists("expenses.csv"):
+    print("Creating 'expenses.csv'...")
+    with open("expenses.csv", "w") as file:
+        writer = csv.DictWriter(file, fieldnames=["item", "price", "category"])
+        writer.writeheader()
+        print("File created successfully")
+else:
+    print("File: 'expenses.csv' not found.")
 
 
         
