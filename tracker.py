ccd5e1bb385de578ef63dc8ac7f3b4f4e412e417
Andrea Caputo <andreacaputo712@gmail.com>
2026-05-01 11:52:00 +0200

    Added logic to prompt user and save data to .csv file

--- tracker.py
+++ tracker.py
@@ -1,25 +1,38 @@
 import os
 import csv
 
+# Declare initial constant variables
 FILE_NAME = "expenses.csv"
-HEADERS = ["item", "price", "category"]
+HEADERS = ["item", "category", "price",]
 
+# Check if file exists; if not, create it
 if not os.path.exists(FILE_NAME):
     print(f"Creating '{FILE_NAME}'...")
     with open(FILE_NAME, "w") as file:
         writer = csv.DictWriter(file, fieldnames=HEADERS)
+        # Create the headers for each 'column'
         writer.writeheader()
         print(f"File: {FILE_NAME} created successfully.")
 else:
-    print(f"File: {FILE_NAME} not found.")
+    print(f"File: {FILE_NAME} ready.")
 
-# --- To do ---
-# 1. Prompt user for 'item' and 'category' (they're both strings)
-# 2. Use while loop to ask for 'price'
-# 3. Try: parse price as float() --> if succesful: break
-# 4. Except: ValueError --> print("Not valid!") --> re-prompt user
-# 4. Open 'expenses.csv' in "a" mode ("append")
-# 5. Write new data as a dictionary row ("item": item, "price": price, "category": category)
+# Prompt user for 'item' and 'category'
+item = input("Item: ")
+category = input("Category (e.g. food, tech, transport): ")
 
+# Use while loop to ask for 'price'; if not valid, re-prompt
+while True:
+    try:
+        price= float(input("Price: "))
+        break
+    except ValueError:
+        print("Invalid. Please enter a valid number: ")
 
-        
+# Open 'expenses.csv' in "a" mode ("append")
+with open(FILE_NAME, "a") as file:
+    writer = csv.DictWriter(file, fieldnames=HEADERS)
+    # Write new data as a dictionary row
+    writer.writerow({"item": item, "category": category, "price": f"${price}"}) # f-string to add '$' sign
+
+print("-- Expense saved successfully! --")
+ 
